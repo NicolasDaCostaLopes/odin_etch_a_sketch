088dc5531aabc3489838659da0c452519490a6b8
Nicolas D - WSL2 <135014303+NicolasDaCostaLopes@users.noreply.github.com>
2024-04-27 19:38:26 +0200

    Implement working Hover & Grid size checking

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".main-container");
+const resetGrid = document.querySelector("#resetGrid");
+const changeGrid = document.querySelector("#changeGrid");
+let gridSize = 16;
 
 const findSquareSize = (sideLenght) => {
     return Math.floor(480 / sideLenght);
@@ -12,6 +15,7 @@ const fillContainer = (sideLenght) => {
     for (y = 0; y < sideLenght; y++) {
         const column = document.createElement("div");
         column.classList.add("flex");
+        column.addEventListener("mouseover", hoverColor);
         for (x = 0; x < sideLenght; x++) {
             const square = document.createElement("div");
             square.classList.add("square");
@@ -27,3 +31,30 @@ const fillContainer = (sideLenght) => {
 const clearContainer = () => {
     container.textContent = "";
 }
+
+const hoverColor = (e) => {
+    e.target.style.backgroundColor = "rgba(255,0,0,0.3)";
+}
+
+const verifyProvidedGridSize = (input) => {
+    input = +input;
+    if (typeof input === "number" && !isNaN(input)) {
+        gridSize = input;
+        return false
+    } else {
+        return true;
+    }
+}
+
+const askGridSize = () => {
+    let answer = prompt("What size would you like ? from 4 up to 100", 16);
+    let keepGoing = verifyProvidedGridSize(answer);
+    while (keepGoing) {
+        answer = prompt(`Invalid answer ${answer} is not from 4 up to 100`, 16);
+        keepGoing = verifyProvidedGridSize(answer);
+    }
+    fillContainer(gridSize);
+}
+fillContainer(gridSize);
+askGridSize()
+
